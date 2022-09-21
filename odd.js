73726d7a9a6f6c181ac91058cf0020a6bc407b9f
kikopocampo <kikopocampo@gmail.com>
2022-09-21 11:23:51 -0600

    odd.js

---
+++ odd.js
@@ -0,0 +1,13 @@
+const isOdd = function (num){
+  return num % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+/*const isOdd = function(num){
+  let verify = num%2!==0
+  return num + " is odd : " + verify;
+}
+
+console.log(isOdd(456));*/
