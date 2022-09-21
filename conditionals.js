c37bbe89276822e1bcfd3d14965cf1ce0840d414
kikopocampo <kikopocampo@gmail.com>
2022-09-20 19:11:47 -0600

    more conditional statements

--- conditionals.js
+++ conditionals.js
@@ -11,7 +11,7 @@ if(cold){
 
 console.log("Now you're ready to go outside.");*/
 
-const temperature = 31;
+/*const temperature = 31;
 
 if(temperature < 0){
   console.log("Make sure you pick out a scarf!");
@@ -21,4 +21,20 @@ if(temperature < 0){
   console.log("Short sleeves are fine.")
 }
 
-console.log("Now you're ready to go outside")
+console.log("Now you're ready to go outside")*/
+
+if (temperature > -40 || temperature > 40){
+  console.log("Maybe going outside isn't a good idea....")
+}
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age > 18){
+  console.log("You are eligible to vote.");
+}
+
+if(!raining){
+  console.log("Leave your umbrella at home!");
+}
+
