20e51dedd5aa39302a656865eb751b0cf11bb2fc
kikopocampo <kikopocampo@gmail.com>
2022-09-22 11:54:02 -0600

    ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,9 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+// adds "root beer" at the end of the Arr
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+// retrieved the first data of the Arr.
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
